e130c8d20abf1cf0fe46f24abddd3d96d93932b9
chuacw <1757930+chuacw@users.noreply.github.com>
2022-06-16 17:50:02 +0800

    Initial check-in
    Initial check-in

---
+++ FindAddress.ts
@@ -0,0 +1,126 @@
+// import { WebSocketProvider } from "@ethersproject/providers";
+// import { ethers } from "ethers";
+import { ParamStr } from "delphirtl/rtl";
+import { ExtractFileDir } from "delphirtl/sysutils";
+import { utils } from "ethers";
+import { USDTABI } from "./consts/USDTABI";
+// import { ParamStr } from "./lib/rtl";
+// import { ExtractFileDir } from "./lib/sysutils";
+
+namespace GetPastLogs {
+    console.log("Token Address Finder v0.3");
+    const abidecoder = require('abi-decoder');
+    const Web3 = require("web3");
+    const path = require("path");
+    const fs = require("fs");
+
+    require('dotenv').config();
+
+    abidecoder.addABI(USDTABI);
+
+    const WebSocketURL = process.env.WebSocketURL || ""; 
+    let lastValue = 0;
+    type callback = () => void;
+    async function SaveSettings(data: string, aCallback?: callback) {
+        const LPath = ExtractFileDir(ParamStr(0));
+        const LFilename = LPath + path.sep + ".env";
+        await fs.writeFile(LFilename, data, () => {
+            if (aCallback) {
+                aCallback();
+            }
+        });
+    }
+
+    async function SaveAddress(aAddr: string) {
+        const LPath = ExtractFileDir(ParamStr(0));
+        const LFilename = LPath + path.sep + "foundAddress.txt";
+        await fs.writeFile(LFilename, `foundAddress=${aAddr}`, () => {});
+    }
+
+    function EnsureWebSocketAvail() {
+        if ((WebSocketURL == undefined)||(WebSocketURL == "")) {
+            throw new Error("WebSocketURL is not set!");
+        }
+    }
+    async function GetPastLogs() {
+        EnsureWebSocketAvail();
+        const addrPrefix = ParamStr(1).toLowerCase();
+        const addrSuffix = ParamStr(2).toLowerCase();
+        // const provider = ethers.providers.getDefaultProvider(WebSocketURL);
+        const web3 = new Web3(WebSocketURL);
+        let lastBlock = process.env.lastValue || await web3.eth.getBlockNumber();
+        let found = false; let breakgetLog = false;
+        const maxBlocks = 3;
+        const Transfer_Event = utils.id("Transfer(address,address,uint256)");
+        const LFilter = {
+            address: ["0xdAC17F958D2ee523a2206206994597C13D831ec7", // USDT
+                      "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"],
+            topics: [
+                [Transfer_Event]
+            ],
+            fromBlock: 1000,
+        }
+        const testlogs = await web3.eth.getPastLogs(LFilter);
+        const decodedLogs = abidecoder.decodeLogs(testlogs);
+        console.log(decodedLogs);
+        console.log("Faster...");
+        while ((!found)&&(!breakgetLog)) {
+            console.log(`Fetching ${lastBlock - maxBlocks} to ${lastBlock}`);
+            try {
+                const logs = await web3.eth.getPastLogs({
+                    fromBlock: lastBlock - maxBlocks,
+                    toBlock: lastBlock,
+                    address: ["0xdAC17F958D2ee523a2206206994597C13D831ec7", // USDT
+                              "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"],
+                    topics: [
+                       [Transfer_Event]
+                    ],
+                                    
+                });
+                lastValue = lastBlock;
+                const decodedLogs = abidecoder.decodeLogs(logs);
+                // console.log(decodedLogs);
+                for (const log of decodedLogs) {
+                    if (log.name != "Transfer")
+                        continue;
+                    let logAddr = log.events[0].value.toLowerCase(); // from
+                    if (logAddr.startsWith(addrPrefix) && logAddr.endsWith(addrSuffix)) {
+                        console.log(`Address is ${logAddr}!`);
+                        found = true;
+                        break;
+                    }
+                    logAddr = log.events[1].value.toLowerCase(); // to
+                    if (logAddr.startsWith(addrPrefix) && logAddr.endsWith(addrSuffix)) {
+                        console.log(`Address is ${logAddr}!`);
+                        SaveAddress(logAddr);
+                        found = true;
+                        break;
+                    }
+                }
+                lastBlock-=maxBlocks;
+            } catch (e) {
+                console.log(`${e}`);
+                console.log("Terminating app.");
+                await SaveSettings(`lastValue=${lastValue}`, () => {
+                    breakgetLog = true;
+                })
+            }
+        }
+        // // const decodedLog = web3.eth.abi.decodeLog(KeepersABI, logs[0].data, logs[0].topics);
+        // // console.log(decodedLog);
+
+        // abidecoder.addABI(KeepersABI);
+        // const decodedLogs = abidecoder.decodeLogs(logs);
+        // for (const log of decodedLogs) {
+        //    console.log(`name: ${log.name}`)
+        //    for (const event of log.events) {
+        //         const value = web3.eth.abi.decodeParameter(event.type, event.value);
+        //         console.log(`name: ${name}, value: ${value}`);
+        //    }
+
+        // }
+
+    }
+
+    GetPastLogs();
+}
---
+++ FindAddressForward.ts
@@ -0,0 +1,132 @@
+// import { WebSocketProvider } from "@ethersproject/providers";
+// import { ethers } from "ethers";
+
+import { utils } from "ethers";
+import { USDTABI } from "./consts/USDTABI";
+import { ExtractFileDir, FileExists } from "delphirtl/SysUtils";
+import { ParamStr, Sleep } from "delphirtl/rtl";
+
+namespace GetPastLogs {
+    console.log("Token Address Finder v0.3");
+    const abidecoder = require('abi-decoder');
+    const Web3 = require("web3");
+    const path = require("path");
+    const fs = require("fs");
+
+    const ENVfilename = path.resolve(__dirname, "forward.env");
+
+    require('dotenv').config({ path: ENVfilename });
+
+    abidecoder.addABI(USDTABI);
+
+    const WebSocketURL = process.env.WebSocketURL || ""; 
+
+    let lastValue = 0;
+    type callback = () => void;
+    let saveSettingsRunning = false;
+    async function SaveSettings(data: string, aCallback?: callback) {
+        if (!saveSettingsRunning) {
+            saveSettingsRunning = true;
+            try {
+                const LPath = ExtractFileDir(ParamStr(0));
+                const LFilename = ENVfilename;
+
+                await fs.writeFile(LFilename, data, () => {
+                    if (aCallback) {
+                        aCallback();
+                    }
+                });
+            } finally {
+                saveSettingsRunning = false;
+            }
+        }
+    }
+
+    const CSavedFileName = path.resolve(ExtractFileDir(ParamStr(0)), "foundAddress.txt")
+
+    async function SaveAddress(aAddr: string) {
+        const LFilename = CSavedFileName;
+        await fs.writeFile(LFilename, `foundAddress=${aAddr}`, () => { });
+    }
+
+    function EnsureWebSocketAvail() {
+        if ((WebSocketURL == undefined)||(WebSocketURL == "")) {
+            console.log("No WebSocketURL specified!");
+            process.exit(2);
+        }
+    }
+    function EnsureSavedFileExists() {
+        if (FileExists(CSavedFileName)) {
+            console.log(`Saved file exists!`);
+            process.exit(1);
+        }
+    }
+
+    async function GetPastLogs() {
+        EnsureSavedFileExists();
+        EnsureWebSocketAvail();
+
+        const addrPrefix = ParamStr(1).toLowerCase();
+        const addrSuffix = ParamStr(2).toLowerCase();
+        const web3 = new Web3(WebSocketURL);
+        let fromBlock = Number.parseInt(process.env.lastValue || "4634748");
+        let found = false; let breakGetLog = false;
+        const maxBlocks = 3;
+        const Transfer_Event = utils.id("Transfer(address,address,uint256)");
+        let foundAddress = "";
+        console.log("Forward...");
+        while ((!found) && (!breakGetLog)) {
+            console.log(`Fetching ${fromBlock} to ${fromBlock + maxBlocks}`);
+            try {
+                const lastBlock = fromBlock + maxBlocks;
+                const logs = await web3.eth.getPastLogs({
+                    fromBlock: fromBlock,
+                    toBlock: lastBlock,
+                    address: ["0xdAC17F958D2ee523a2206206994597C13D831ec7", // USDT
+                        "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"],
+                    topics: [
+                        [Transfer_Event]
+                    ],
+
+                });
+                lastValue = lastBlock;
+                const decodedLogs = abidecoder.decodeLogs(logs);
+                // console.log(decodedLogs);
+                for (const log of decodedLogs) {
+                    if (log.name != "Transfer")
+                        continue;
+                    let logAddr = log.events[0].value.toLowerCase(); // from
+                    if (logAddr.startsWith(addrPrefix) && logAddr.endsWith(addrSuffix)) {
+                        foundAddress = logAddr;
+                        found = true;
+                        break;
+                    }
+                    logAddr = log.events[1].value.toLowerCase(); // to
+                    if (logAddr.startsWith(addrPrefix) && logAddr.endsWith(addrSuffix)) {
+                        console.log(`Address is ${logAddr}!`);
+                        found = true;
+                        break;
+                    }
+                }
+                fromBlock += maxBlocks + 1;
+            } catch (e) {
+                breakGetLog = true;
+                console.log(`${e}`);
+                console.log(new Date().toString());
+                console.log("Terminating app.");
+                await SaveSettings(`lastValue=${lastValue}\nWebSocketURL="${WebSocketURL}"`, () => {
+                    breakGetLog = true;
+                });
+                await Sleep(100);
+            }
+        }
+        if (found) {
+            console.log(`Address is ${foundAddress}!`);
+            await SaveAddress(foundAddress);
+        }
+        await Sleep(100);
+        process.exit(0);
+    }
+
+    GetPastLogs();
+}
